827e6f2c42a6d4043f876e79fafa5cfa844ddd95
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-07-16 10:35:03 +0530

    Add columns to inventory migration

--- database/migrations/2020_07_16_045651_create_inventories_table.php
+++ database/migrations/2020_07_16_045651_create_inventories_table.php
@@ -15,6 +15,8 @@ class CreateInventoriesTable extends Migration
     {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
             $table->timestamps();
         });
     }
