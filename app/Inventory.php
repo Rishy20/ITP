63e0dea25fec5f46dfaf69452559ad07b93f852c
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-07-17 23:32:02 +0530

    Allow mass assignment for properties of inventory model

--- app/Inventory.php
+++ app/Inventory.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    protected $fillable = ['name', 'address'];  // Allow mass assignment for name and address properties
 }
