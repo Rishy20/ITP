86ddf8897840edb11cdfc5620296eb030d771767
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2020-07-20 11:44:27 +0530

    All page and Add page Template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700,400&display=swap" rel="stylesheet">
+
     <link href="icons/css/all.css" rel="stylesheet">
     <link href="css/sample.css" rel="stylesheet">
     <link href="css/styles.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="vendor/DataTables/datatables.min.css"/>
 </head>
 <body>
-    @include('header')
-    @include('navbar')
+    @include('assets.header')
+    @include('assets.navbar')
     <div class="main">
         @yield('content')
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="vendor/DataTables/datatables.min.js"></script>
+    <script>
+    $(document).ready( function () {
+        $('#myTable').DataTable({
+            "dom": '<"top"f><t><"bottom"lip>',
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "🔎 Search"
+                }
+        });
+    } );
+</script>
 </body>
 </html>
