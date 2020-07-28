a837c0781e709db2fc0838353505c5f39412e958
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-07-28 20:24:03 +0530

    Create migration for inventory_items table

---
+++ database/migrations/2020_07_28_142643_create_inventory_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_items', function (Blueprint $table) {
+            $table->bigInteger('inventory_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+            $table->integer('qty')->default(0);
+            $table->timestamps();
+            $table->primary(['inventory_id', 'product_id']);
+            $table->foreign('inventory_id')->references('id')->on('inventories')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_items');
+    }
+}
