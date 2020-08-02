2dcd77dfd42c81a04949302d474854ece4eb25fe
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2020-08-03 01:14:36 +0530

    Commit before pull Products

--- resources/views/assets/header.blade.php
+++ resources/views/assets/header.blade.php
@@ -1,12 +1,13 @@
+@livewireStyles
 <header>
     <div class="header">
             <div class="header-content">
                 <div class="header-store">Leatherline</div>
-                <div class="header-time"><livewire:time /></div>
+                <div class="header-time"><livewire:time/></div>
                 <div class="header-user">
                     <i class="fas fa-user-circle header-icon"></i> Rishard Akram
                 </div>
             </div>
     </div>
 </header>
-
+@livewireScripts
