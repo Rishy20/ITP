99d58a22fd97a17cfc73b47d07e1fd3eeec61517
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-08-09 09:49:52 +0530

    Generate model class, controller class and migration files for stock transfers

---
+++ app/Http/Controllers/StockTransferController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Inventory;
+use App\Product;
+use App\StockTransfer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class StockTransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StockTransfer  $stockTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StockTransfer $stockTransfer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StockTransfer  $stockTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StockTransfer $stockTransfer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StockTransfer  $stockTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StockTransfer $stockTransfer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StockTransfer  $stockTransfer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StockTransfer $stockTransfer)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_08_07_103007_create_stock_transfers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTransfersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_transfers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_transfers');
+    }
+}
