c793bfc3cdb471e537c1dff01e1c0bee5a5fc3e5
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-09-07 20:06:14 +0530

    Generate model class, controller class and migration files for inventory counts

---
+++ app/Http/Controllers/InventoryCountController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InventoryCount;
+use Illuminate\Http\Request;
+
+class InventoryCountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\InventoryCount  $inventoryCount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InventoryCount $inventoryCount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\InventoryCount  $inventoryCount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InventoryCount $inventoryCount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\InventoryCount  $inventoryCount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InventoryCount $inventoryCount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\InventoryCount  $inventoryCount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InventoryCount $inventoryCount)
+    {
+        //
+    }
+}
