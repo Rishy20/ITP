d65229900ff054e8df269fbcb162e8dc093ee5a5
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-10-07 23:25:43 +0530

    Overhaul stock transfer implementation with JS

--- app/Http/Livewire/StockTransfers/StockTransferForm.php
+++ app/Http/Livewire/StockTransfers/StockTransferForm.php
@@ -10,32 +10,21 @@ use Livewire\WithPagination;
 
 class StockTransferForm extends Component
 {
-    use WithPagination;
-
     public $source;
     public $destination;
     public $inventories;
-    public $search;
-    public $transfer_items;
 
 
     public function mount() {
-        $this->transfer_items = array();
-
         // Get inventories from database
         $this->inventories = Inventory::all();
     }
 
 
-    public function addTransferItem($product_id) {
-        // Check if the item is already added
-        foreach ($this->transfer_items as $transfer_item) {
-            if ($transfer_item == $product_id)
-                return;
-        }
-
-        // Add inventory id and product id as an array to the transfer_items 2D array
-        array_push($this->transfer_items, $product_id);
+    public function swapInventories() {
+        $temp = $this->source;
+        $this->source = $this->destination;
+        $this->destination = $temp;
     }
 
 
@@ -45,7 +34,7 @@ class StockTransferForm extends Component
         $product_ids = $inventory_items->pluck('product_id');
 
         // Get products matching the product ids and the search query
-        $products = Product::whereIn('id', $product_ids)->where('name', 'LIKE', '%'.$this->search.'%')->paginate(10);
+        $products = Product::whereIn('id', $product_ids)->get();
 
         // Assign each product to the corresponding inventory item (check for matching product ids)
         foreach ($inventory_items as $inventory_item) {
@@ -65,46 +54,7 @@ class StockTransferForm extends Component
             }
         }
 
-        // Remove inventory items that does not have an associated product (ones that don't match the search query)
-        foreach ($inventory_items as $key => $inventory_item)
-            if (!(isset($inventory_item->product)))
-                $inventory_items->forget($key);
-
-        // Remove inventory items that have already been added
-        foreach ($inventory_items as $key=> $inventory_item) {
-            foreach ($this->transfer_items as $transfer_item) {
-                if ($transfer_item == $inventory_item->product->id)
-                    $inventory_items->forget($key);
-            }
-        }
-
         return ['inventory_items' => $inventory_items, 'products' => $products];
-    }
-
-
-    private function getTransferItems() {
-        // Create transfer item objects array
-        $transfer_item_objects = array();
-
-        // Assign to each transfer item, its respective product and destination qty
-        if ($this->transfer_items > 0) {
-            foreach ($this->transfer_items as $transfer_item) {
-                $inventory_item = DB::table('inventory_items')->where('inventory_id', $this->source)
-                    ->where('product_id', $transfer_item)->first();  // Get respective inventory item object
-                $inventory_item->product = Product::find($transfer_item);  // Get respective product
-                $inventory_item->destination_qty = DB::table('inventory_items')
-                    ->where('inventory_id', $this->destination)->where('product_id', $transfer_item)
-                    ->pluck('qty')->first();  // Get respective destination qty
-
-                // If destination qty is null, assign 0
-                if (!$inventory_item->destination_qty)
-                    $inventory_item->destination_qty = 0;
-
-                array_push($transfer_item_objects, $inventory_item);  // Add the assigned transfer item to array
-            }
-        }
-
-        return $transfer_item_objects;
     }
 
 
@@ -121,14 +71,13 @@ class StockTransferForm extends Component
         else
             return view('livewire.stock-transfers.not-enough-inventories');
 
-        // Inventory Items Search
+        // Get inventory items
         $inventory_items = $this->getInventoryItems()['inventory_items'];  // Get inventory items for search
-        $products = $this->getInventoryItems()['products'];  // Get products for pagination
+        $products = $this->getInventoryItems()['products'];
 
-        // Transfer Items Table
-        $transfer_item_objects = $this->getTransferItems();  // Get transfer items to use in Transfer Items table
+        $this->dispatchBrowserEvent('contentChanged');
 
         return view('livewire.stock-transfers.stock-transfer-form')->with('inventory_items', $inventory_items)
-            ->with('products', $products)->with('transfer_item_objects', $transfer_item_objects);
+            ->with('products', $products);
     }
 }
