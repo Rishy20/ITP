b1c4ab07cfea55f96b50a694ec29b810e91da6b9
SandaruLJ <sandarulakshitha1999@gmail.com>
2020-10-08 10:25:53 +0530

    Overhaul inventory count implementation with JS

--- app/Http/Livewire/InventoryCounts/InventoryCountForm.php
+++ app/Http/Livewire/InventoryCounts/InventoryCountForm.php
@@ -6,35 +6,16 @@ use App\Inventory;
 use App\Product;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class InventoryCountForm extends Component
 {
-    use WithPagination;
-
     public $outlet;
     public $inventories;
-    public $search;
-    public $counted_items;
 
 
     public function mount() {
-        $this->counted_items = array();
-
         // Get inventories from database
         $this->inventories = Inventory::all();
-    }
-
-
-    public function addCountedItem($product_id) {
-        // Check if the item is already added
-        foreach ($this->counted_items as $counted_item) {
-            if ($counted_item == $product_id)
-                return;
-        }
-
-        // Add inventory id and product id as an array to the counted_items 2D array
-        array_push($this->counted_items, $product_id);
     }
 
 
@@ -44,7 +25,7 @@ class InventoryCountForm extends Component
         $product_ids = $inventory_items->pluck('product_id');
 
         // Get products matching the product ids and the search query
-        $products = Product::whereIn('id', $product_ids)->where('name', 'LIKE', '%'.$this->search.'%')->paginate(10);
+        $products = Product::whereIn('id', $product_ids)->get();
 
         // Assign each product to the corresponding inventory item (check for matching product ids)
         foreach ($inventory_items as $inventory_item) {
@@ -55,39 +36,7 @@ class InventoryCountForm extends Component
             }
         }
 
-        // Remove inventory items that does not have an associated product (ones that don't match the search query)
-        foreach ($inventory_items as $key => $inventory_item)
-            if (!(isset($inventory_item->product)))
-                $inventory_items->forget($key);
-
-        // Remove inventory items that have already been counted
-        foreach ($inventory_items as $key=> $inventory_item) {
-            foreach ($this->counted_items as $counted_item) {
-                if ($counted_item == $inventory_item->product->id)
-                    $inventory_items->forget($key);
-            }
-        }
-
-        return ['inventory_items' => $inventory_items, 'products' => $products];
-    }
-
-
-    private function getCountedItems() {
-        // Create transfer item objects array
-        $counted_item_objects = array();
-
-        // Assign to each transfer item, its respective product
-        if ($this->counted_items > 0) {
-            foreach ($this->counted_items as $counted_item) {
-                $inventory_item = DB::table('inventory_items')->where('inventory_id', $this->outlet)
-                    ->where('product_id', $counted_item)->first();  // Get respective inventory item object
-                $inventory_item->product = Product::find($counted_item);  // Get respective product
-
-                array_push($counted_item_objects, $inventory_item);  // Add the assigned transfer item to array
-            }
-        }
-
-        return $counted_item_objects;
+        return $inventory_items;
     }
 
 
@@ -103,14 +52,11 @@ class InventoryCountForm extends Component
         else
             return view('livewire.inventory-counts.not-enough-inventories');
 
-        // Inventory Items Search
-        $inventory_items = $this->getInventoryItems()['inventory_items'];  // Get inventory items for search
-        $products = $this->getInventoryItems()['products'];  // Get products for pagination
+        // Get inventory items
+        $inventory_items = $this->getInventoryItems();
 
-        // Counted Items Table
-        $counted_item_objects = $this->getCountedItems();  // Get counted items to use in Counted Items table
+        $this->dispatchBrowserEvent('contentChanged');  // Fire browser event to refresh data
 
-        return view('livewire.inventory-counts.inventory-count-form')->with('inventory_items', $inventory_items)
-            ->with('products', $products)->with('counted_item_objects', $counted_item_objects);
+        return view('livewire.inventory-counts.inventory-count-form')->with('inventory_items', $inventory_items);
     }
 }
